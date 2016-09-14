6ba69c5da36708653d62546cb3e7f1160bc43b26
Rainor <caodong1991@gmail.com>
2016-09-14 15:40:27 +0800

    自动引用计数

--- Automatic Reference Counting.playground/Contents.swift
+++ Automatic Reference Counting.playground/Contents.swift
@@ -28,3 +28,52 @@ reference2 = nil
 reference3 = nil
 
 // 类实例之间的循环强引用
+// 循环强引用：如果两个类实例互相持有对方的强引用，因而每个实例都让对方一直存在。
+
+class Person1 {
+    let name: String
+    init(name: String) {
+        self.name = name
+    }
+    var apartment: Apartment?
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+
+}
+class Apartment {
+    let unit: String
+    init(unit: String) {
+        self.unit = unit
+    }
+    var tenant: Person1?
+    deinit {
+        print("Apartment \(unit) is being deinitialized")
+    }
+}
+
+var john: Person1?
+var unit4A: Apartment?
+john = Person1(name: "john Appleseed")
+unit4A = Apartment(unit: "4A")
+
+john!.apartment = unit4A
+unit4A!.tenant = john
+
+john = nil
+unit4A = nil
+
+// 解决实例之间的循环强引用
+// Swift提供了两种办法来解决在使用类的属性时所遇到的循环强引用问题：若引用（weak reference）和无主引用（unowned reference）。
+// 弱引用和wu
+
+
+
+
+
+
+
+
+
+
+
