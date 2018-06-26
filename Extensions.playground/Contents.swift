c13e5d19f880077c106d5db6cd07c4dcf80085dd
Cao Dong <caodong1991@gmail.com>
2018-06-26 18:31:35 +0800

    创建

---
+++ Extensions.playground/Contents.swift
@@ -0,0 +1,12 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+extension Double {
+    var km: Double { return self * 1_000.0 }
+    var m: Double { return self }
+    var cm: Double { return self / 100.0 }
+    
+    
+}
+
