eeb84b0eb523d625ed83db908c7e4efdf9a4f7a0
Cao Dong <caodong1991@gmail.com>
2018-07-12 18:16:57 +0800

    协议

--- Protocols.playground/Contents.swift
+++ Protocols.playground/Contents.swift
@@ -2,6 +2,71 @@
 
 import Cocoa
 
+// 语法
+//protocol SomeProtocol {
+//
+//}
+
+//struct SomeStructure: FirstProtocol, AnotherProtocol {
+//
+//}
+
+//class SomeClass: SomeSuperclass, FirstProtocol, AnotherProtocol {
+//    
+//}
+
 // 属性要求
 
+//protocol SomeProtocol {
+//    var mustBeSettable: Int { get set}
+//    var doseNotNeedToBeSettable: Int { get }
+//}
 
+protocol FullyNamed {
+    var fullName: String { get }
+}
+
+struct Person: FullyNamed {
+    var fullName: String
+}
+
+let john = Person(fullName: "John Appleseed")
+
+print(john)
+
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    var fullName: String {
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+print(ncc1701.fullName)
+
+// 方法要求
+protocol RandomNumberGenerator {
+    func random() -> Double
+}
+
+class LinearCongruentialGenerator: RandomNumberGenerator {
+    var lastRandom = 42.0
+    let m = 139968.0
+    let a = 3877.0
+    let c = 29573.0
+    func random() -> Double {
+        lastRandom = (lastRandom * a + c).truncatingRemainder(dividingBy: m)
+        return lastRandom / m
+    }
+}
+
+let generator = LinearCongruentialGenerator()
+print("Here's a random number:\(generator.random())")
+print("And another one: \(generator.random())")
+
+// 异变方法需求
