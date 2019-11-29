59a0292d43bf84858dc4f2dc3d7dec743f7b847c
CaoDong <caodong1991@gmail.com>
2019-11-29 19:47:52 +0800

    嵌套类型 更新到5.1版本

--- 19 - NestedType.playground/Contents.swift
+++ 19 - NestedType.playground/Contents.swift
@@ -2,21 +2,31 @@
 
 import UIKit
 
+// 嵌套类型
+/*
+ 枚举常用于为特定类或结构体实现某些功能。
+ 类似地，枚举可以方便的定义工具类或结构体，从而为某个复杂的类型所使用。
+ 为了实现这种功能，Swift允许你定义嵌套类型，可以在支持的类型中定义嵌套的枚举、类和结构体。
+ 要在一个类型中嵌套另一个类型，将嵌套类型的定义写在其外部类型的{}内，而且可以根据需要定义多级嵌套。
+ */
+
+// 嵌套类型实践
+
 struct BlackjackCard {
     enum Suit: Character {
-        case Spades = "♠", Hearts = "♡", Diamonds = "♢", Clubs = "♣"
+        case spades = "♠", hearts = "♡", diamonds = "♢", clubs = "♣"
     }
     enum Rank: Int {
-        case Two = 2, Three, Four, Five, Six, Seven, Eight, Nine, Ten
-        case Jack, Queen, King, Ace
+        case two = 2, three, four, five, six, seven, eight, nine, ten
+        case jack, queen, king, ace
         struct Values {
             let first: Int, second: Int?
         }
         var values: Values {
             switch self {
-            case .Ace:
+            case .ace:
                 return Values(first: 1, second: 11)
-            case .Jack, .Queen, .King:
+            case .jack, .queen, .king:
                 return Values(first: 10, second: nil)
             default:
                 return Values(first: self.rawValue, second: nil)
@@ -35,3 +45,12 @@ struct BlackjackCard {
         return output
     }
 }
+
+let theAceOfSpades = BlackjackCard(rank: .ace, suit: .spades)
+print("theAceOfSpades: \(theAceOfSpades.description)")
+
+// 引用嵌套类型
+/*
+ 在外部引用嵌套类型时，在嵌套类型的类型名前加上其外部类型的类型名作为前缀。
+ */
+let heartsSymbol = BlackjackCard.Suit.hearts.rawValue
