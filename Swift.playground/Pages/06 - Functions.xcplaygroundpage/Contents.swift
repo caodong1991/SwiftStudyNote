14d6f7c6f7a7813c51f3a3f7118cf9a8b8f686b3
CaoDong <caodong1991@gmail.com>
2020-12-07 10:15:43 +0800

    高阶函数补充

--- Swift.playground/Pages/06 - Functions.xcplaygroundpage/Contents.swift
+++ Swift.playground/Pages/06 - Functions.xcplaygroundpage/Contents.swift
@@ -355,3 +355,43 @@ while anotherCurrentValue != 0 {
 }
 print("zero!")
 
+
+// 高阶函数
+// mao
+/*
+ 对于原始集合里的每一个元素，以一个变换后的元素替换之形成一个新的集合
+ */
+let numbers = [1, 2, 4, 5, 10]
+print(numbers.map { $0 * 10 })
+
+// filter
+/*
+ 对于原始集合里的每一个元素，通过判定来将其丢弃或者放进新集合
+ */
+print(numbers.filter { $0 > 4 })
+
+// reduce
+/*
+ 对于原始集合里的每一个元素，作用于当前累积的结果上
+ */
+print(numbers.reduce(100) { $0 + $1 })
+
+// flatMap
+/*
+ 对于元素是集合的集合，可以得到单级的集合
+ */
+let results = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+let allResults = results.flatMap { $0.map { $0 * 10 } }
+let passMarks = results.flatMap { $0.filter { $0 > 5 } }
+print(allResults)
+print(passMarks)
+
+// compactMap
+/*
+ 过滤空值
+ */
+let keys: [String?] = ["zhangsan", nil, "lisi", nil, "wangwu"]
+let validNames = keys.compactMap { $0 }
+print(validNames)
+let counts = keys.compactMap { $0?.count }
+print(counts)
